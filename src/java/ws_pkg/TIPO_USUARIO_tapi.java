98474e4b39c093f5e3c7245a7cced9261efd439c
eduardo bravo <eduardobravod@gmail.com>
2017-10-18 19:36:46 -0300

    update ws TIPO_USUARIO_tapi

--- src/java/ws_pkg/TIPO_USUARIO_tapi.java
+++ src/java/ws_pkg/TIPO_USUARIO_tapi.java
@@ -5,9 +5,13 @@
  */
 package ws_pkg;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import pkgBLL.PRM;
+import pkgDal.DAL_coneccion;
 
 /**
  *
@@ -17,10 +21,70 @@ import javax.jws.WebParam;
 public class TIPO_USUARIO_tapi {
 
     /**
-     * This is a sample web service operation
+     * Lista completa
+     * @return Json
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "sel")
+    public String sel() throws Exception {
+        List<PRM> lp = new ArrayList<>();
+        return DAL_coneccion.retornaJSON("TIPO_USUARIO_tapi.sel", lp);
+    }
+    /**
+     * Selección por ID
+     * @param id
+     * @return Json
+     */
+    @WebMethod(operationName = "selId")
+    public String selId(@WebParam(name = "id") String id) throws Exception {
+        List<PRM> lp = new ArrayList<>();
+        lp.add(new PRM(1,"String",id));
+        return DAL_coneccion.retornaJSON("TIPO_USUARIO_tapi.selId", lp);
+    }
+    /**
+     * Crear registro
+     * @param p_GLOSA_TIPO_USUARIO
+     * @param p_RUT_CREACION
+     * @param p_ID_TIPO_USUARIO
+     * @return número de filas afectadas
+     */
+    @WebMethod(operationName = "ins")
+    public int ins(
+            @WebParam(name = "p_GLOSA_TIPO_USUARIO") String p_GLOSA_TIPO_USUARIO,
+            @WebParam(name = "p_RUT_CREACION") String p_RUT_CREACION,
+            @WebParam(name = "p_ID_TIPO_USUARIO") String p_ID_TIPO_USUARIO) throws Exception {
+        List<PRM> lp = new ArrayList<>();
+        lp.add(new PRM(1,"String",p_GLOSA_TIPO_USUARIO));
+        lp.add(new PRM(1,"String",p_RUT_CREACION));
+        lp.add(new PRM(1,"String",p_ID_TIPO_USUARIO));
+        return DAL_coneccion.ejecutaTAPI("TIPO_USUARIO_tapi.ins", lp);
+    }
+    /**
+     * Actualizar registro
+     * @param p_RUT_ACTUALIZACION
+     * @param p_GLOSA_TIPO_USUARIO
+     * @param p_ID_TIPO_USUARIO
+     * @return número de filas afectadas
+     */
+    @WebMethod(operationName = "upd")
+    public int upd(
+            @WebParam(name = "p_RUT_ACTUALIZACION") String p_RUT_ACTUALIZACION,
+            @WebParam(name = "p_GLOSA_TIPO_USUARIO") String p_GLOSA_TIPO_USUARIO,
+            @WebParam(name = "p_ID_TIPO_USUARIO") String p_ID_TIPO_USUARIO) throws Exception {
+        List<PRM> lp = new ArrayList<>();
+        lp.add(new PRM(1,"String",p_RUT_ACTUALIZACION));
+        lp.add(new PRM(1,"String",p_GLOSA_TIPO_USUARIO));
+        lp.add(new PRM(1,"String",p_ID_TIPO_USUARIO));
+        return DAL_coneccion.ejecutaTAPI("TIPO_USUARIO_tapi.upd", lp);
+    }
+    /**
+     * Eliminar registro
+     * @param id
+     * @return número de filas afectadas
+     */
+    @WebMethod(operationName = "del")
+    public int del(@WebParam(name = "id") String id) throws Exception {
+        List<PRM> lp = new ArrayList<>();
+        lp.add(new PRM(1,"Integer",id));
+        return DAL_coneccion.ejecutaTAPI("TIPO_USUARIO_tapi.del", lp);
     }
 }
