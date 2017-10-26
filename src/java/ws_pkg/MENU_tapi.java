abb15a0761e37672aad495d8e52cad939e579cb2
eduardo bravo <eduardobravod@gmail.com>
2017-10-26 15:09:59 -0300

    upd ws MENU_tapi INS return ID in Json

--- src/java/ws_pkg/MENU_tapi.java
+++ src/java/ws_pkg/MENU_tapi.java
@@ -43,23 +43,20 @@ public class MENU_tapi {
     /**
      * Crear registro
      * @param p_RUT_CREACION
-     * @param p_ID_MENU
      * @param p_GLOSA_MENU
      * @param p_PAGINA
      * @return número de filas afectadas
      */
     @WebMethod(operationName = "m_ins")
-    public int m_ins(
+    public String m_ins(
             @WebParam(name = "p_RUT_CREACION") String p_RUT_CREACION,
-            @WebParam(name = "p_ID_MENU") String p_ID_MENU,
             @WebParam(name = "p_GLOSA_MENU") String p_GLOSA_MENU,
             @WebParam(name = "p_PAGINA") String p_PAGINA) throws Exception {
         List<PRM> lp = new ArrayList<>();
         lp.add(new PRM(1,"String",p_RUT_CREACION));
-        lp.add(new PRM(2,"Int",p_ID_MENU));
-        lp.add(new PRM(3,"String",p_GLOSA_MENU));
-        lp.add(new PRM(4,"String",p_PAGINA));
-        return DAL_coneccion.ejecutaTAPI("MENU_tapi.ins", lp);
+        lp.add(new PRM(2,"String",p_GLOSA_MENU));
+        lp.add(new PRM(3,"String",p_PAGINA));
+        return DAL_coneccion.retornaJSON("MENU_tapi.ins", lp);
     }
     /**
      * Actualizar registro
